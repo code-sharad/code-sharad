003cb646fde8c919a96e2a29636e9a6b5ab31c9a
code-sharad <codesharad@gmail.com>
2023-08-24 08:33:25 +0530

    page loading animation added

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
 let tl = gsap.timeline();
 
-const body = document.querySelector('body');
-
-body.style.webkitFilter='blur(50px)';
-body.style.transition=' 0.5s'
-
-setTimeout(() => {
-    body.style.webkitFilter='blur(0)';
-},800)
-
 
 
 tl.from("a svg",{
