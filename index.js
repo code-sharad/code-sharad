690a2eeafe049e514ebb3a3fac4c000420704752
code-sharad <codesharad@gmail.com>
2023-08-24 07:57:35 +0530

    page loading animation added

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 let tl = gsap.timeline();
 
-t1.from('img',{
-    delay:0.5,
-})
+const body = document.querySelector('body');
 
-tl.from("a svg",{
-    y:-3,
-    repeat:-1,
-    yoyo:true
-})
+setTimeout(() => {
+    body.style.webkitFilter='blur(0)';
+},1000)
 
-tl.from("#profile-image",{
-   
-})
+body.style.webkitFilter='blur(50px)';
+body.style.transition='ease 0.25s'
+
+// t1.from('img',{
+//     delay:0.5,
+// })
+
+// tl.from("a svg",{
+//     y:-3,
+//     repeat:-1,
+//     yoyo:true
+// })
+
